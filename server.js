e7c18bca1e1937bc8a5f78349746c51d256bb96f
mohammadsweiti <mohammadsweity57669@gmail.com>
2024-12-26 02:02:02 +0200

    Feat:server setyp is ready

---
+++ server.js
@@ -0,0 +1,46 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// Start up an instance of app
+const app = express();
+
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 8855;
+const server = app.listen(port,()=>{
+    console.log(`port running in port whis is ${port}`);
+    
+});
+
+app.get('/generalthing',(request,response)=>{
+    response.send(projectData);
+});
+
+app.post('/generalthing',(request,response)=>{
+    console.log(`server data is ${request.body}`);
+    const newObject ={
+        date : request.body.date,
+        temperature : request.body.temperature,
+        feeling : request.body.feeling
+    }
+    projectData.push(newObject);
+    response.send(projectData);   
+})
+
+
+
---
+++ website/app.js
@@ -0,0 +1,2 @@
+let date = new Date();
+let newData = date.getMonth()+'/'+date.getDate()+'/'+date.getFullYear();
